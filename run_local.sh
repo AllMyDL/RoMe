9852618829ef929e958113e4351537694b7d3673
aqiuxx <qiu_zhi_chang@126.com>
2026-01-03 02:58:53 +0000

    init_nuscenes

--- run_local.sh
+++ run_local.sh
@@ -1,8 +1,8 @@
-export WANDB_BASE_URL="######:8080"
-export WANDB_API_KEY="local-######"
+export WANDB_BASE_URL="https://api.wandb.ai"
+export WANDB_API_KEY="3b8190fcd4e64b10225bdf05797f4326836e6f1f"
 # export WANDB_MODE="offline"
 export PYTHONPATH=${PWD}
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 # python3 scripts/train.py --config configs/local_nusc_mini.yaml
 python3 scripts/train.py --config configs/local_nusc.yaml
